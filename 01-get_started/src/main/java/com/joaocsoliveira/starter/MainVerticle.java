e90742e8a891f328e0fdada2781b6ae83bc72aaa
Joao Oliveira <jcsodelta@gmail.com>
2024-01-19 12:01:28 -0300

    update 01-get_started

--- 01-get_started/src/main/java/com/joaocsoliveira/starter/MainVerticle.java
+++ 01-get_started/src/main/java/com/joaocsoliveira/starter/MainVerticle.java
@@ -1,23 +1,38 @@
 package com.joaocsoliveira.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+    Router router = Router.router(vertx);
+
+    router.route().handler(context -> {
+      String address = context.request().connection().remoteAddress().toString();
+      MultiMap queryParams = context.queryParams();
+      String name = queryParams.contains("name") ? queryParams.get("name") : "stranger";
+      context.json(
+        new JsonObject()
+          .put("name", name)
+          .put("address", address)
+          .put("message", String.format("Hello %s from %s!", name, address))
+      );
     });
+
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(8888, http -> {
+        if (http.succeeded()) {
+          startPromise.complete();
+          System.out.println("HTTP server started on port 8888");
+        } else {
+          startPromise.fail(http.cause());
+        }
+      });
   }
 }
--- README.md
+++ README.md
@@ -5,14 +5,9 @@ Following tutorials at https://vertx.io/
 
 ## Objectives
 
-1) Test Asynchronous models:
-- Callbacks
-- Promises/Futures
-- RxJava
-- Kotlin coroutines
-
+1) Do tutorials.
 2) Test behavior on multiple requests.
 3) Test behavior on multiple connections.
 4) Test circuit breaker.
-5) Look at https://reactiverse.io/ projects
-6) Look at https://github.com/vert-x3/vertx-awesome projects
+5) Look at https://reactiverse.io/ projects.
+6) Look at https://github.com/vert-x3/vertx-awesome projects.
